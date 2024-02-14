0e70dcce0ef0ada91d582d01f62a4d3049fcc68b
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-14 17:54:15 +0330

    add start_time

--- Multi-Threading/001-Threading/02-task.py
+++ Multi-Threading/001-Threading/02-task.py
@@ -8,8 +8,12 @@ def task(id):
     print(f'the task {id} completed')
 
 
+start_time = perf_counter()
+
+# create and start 10 threads
 threads = []
 for n in range(1, 11):
     t = Thread(target=task, args=(n,))
     threads.append(t)
     t.start()
+
