ec1bd9c05bd7927e35fff921f2af880a22776d1c
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-15 20:48:13 +0330

    add ShowThread class

--- Multi-Threading/002-Thread-Class/01-show.py
+++ Multi-Threading/002-Thread-Class/01-show.py
@@ -6,3 +6,13 @@ def show(name, delay):
     print(f'start {name}')
     sleep(delay)
     print(f'stop {name}')
+
+
+class ShowThread(Thread):
+    def __init__(self, name, delay):
+        super().__init__()
+        self.name = name
+        self.delay = delay
+
+    def run(self):
+        show(self.name, self.delay)
