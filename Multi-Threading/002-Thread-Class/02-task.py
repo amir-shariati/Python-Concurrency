51823ee310492001348dc6edcc71273f86036cb9
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-15 21:00:19 +0330

    add task func

--- Multi-Threading/002-Thread-Class/02-task.py
+++ Multi-Threading/002-Thread-Class/02-task.py
@@ -1,2 +1,8 @@
 from time import sleep, perf_counter
 from threading import Thread
+
+
+def task(id):
+    print(f'starting the task {id} \n')
+    sleep(3)
+    print(f'the task {id} completed \n')
