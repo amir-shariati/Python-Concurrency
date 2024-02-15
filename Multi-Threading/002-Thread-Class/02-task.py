a670ba86d0e64e927fd71d3819dd6f7a45a9d25d
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-15 21:04:07 +0330

    add TaskThread class

--- Multi-Threading/002-Thread-Class/02-task.py
+++ Multi-Threading/002-Thread-Class/02-task.py
@@ -6,3 +6,12 @@ def task(id):
     print(f'starting the task {id} \n')
     sleep(3)
     print(f'the task {id} completed \n')
+
+
+class TaskThread(Thread):
+    def __init__(self, task_id):
+        super().__init__()
+        self.task_id = task_id
+
+    def run(self):
+        task(self.task_id)
