7a6fcbbc0c1023ea0f7bc215425ca6abf24a7c67
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-17 17:39:33 +0330

    add sed request to run method

--- Multi-Threading/003-Return-Values-from-Thread/02-stock.py
+++ Multi-Threading/003-Return-Values-from-Thread/02-stock.py
@@ -14,4 +14,4 @@ class Stock(Thread):
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
         }
-
+        response = requests.get(self.url, headers=headers)
