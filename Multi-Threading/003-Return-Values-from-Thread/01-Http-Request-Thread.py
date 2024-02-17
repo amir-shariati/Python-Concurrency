09c4262c816898148b61a49190f57be271b31c15
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-17 16:32:59 +0330

    add HttpRequestThread

--- Multi-Threading/003-Return-Values-from-Thread/01-Http-Request-Thread.py
+++ Multi-Threading/003-Return-Values-from-Thread/01-Http-Request-Thread.py
@@ -2,3 +2,20 @@ from time import perf_counter
 from threading import Thread
 import urllib.request
 import urllib.error
+
+
+class HttpRequestThread(Thread):
+    def __init__(self, url: str) -> None:
+        super().__init__()
+        self.url = url
+        self.http_status_code = None
+        self.reason = None
+
+    def run(self) -> None:
+        try:
+            response = urllib.request.urlopen(self.url)
+            self.http_status_code = response.code
+        except urllib.error.HTTPError as e:
+            self.http_status_code = e.code
+        except urllib.error.URLError as e:
+            self.reason = e.reason
