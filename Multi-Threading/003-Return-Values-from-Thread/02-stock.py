d62747ac1663594b60d624c8b59152f9ab99677e
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-17 17:41:03 +0330

    find price from html

--- Multi-Threading/003-Return-Values-from-Thread/02-stock.py
+++ Multi-Threading/003-Return-Values-from-Thread/02-stock.py
@@ -15,3 +15,14 @@ class Stock(Thread):
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
         }
         response = requests.get(self.url, headers=headers)
+        if response.status_code == 200:
+            # parse the HTML
+            tree = html.fromstring(response.text)
+            # get the price in text
+            price_text = tree.xpath(
+                '//*[@id="quote-header-info"]/div[3]/div[1]/div[1]/fin-streamer[1]/text()')
+            if price_text:
+                try:
+                    self.price = float(price_text[0].replace(',', ''))
+                except ValueError:
+                    self.price = None
