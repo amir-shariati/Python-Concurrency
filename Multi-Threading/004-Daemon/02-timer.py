6d4f7718d5a1cf3022f9a295c2972cbe0d2acb27
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-21 18:46:43 +0330

    add show_timer

--- Multi-Threading/004-Daemon/02-timer.py
+++ Multi-Threading/004-Daemon/02-timer.py
@@ -1,2 +1,10 @@
 from time import sleep
 from threading import Thread
+
+
+def show_timer():
+    count = 0
+    while True:
+        count += 1
+        sleep(1)
+        print(f'Has been waiting for {count} second(s)...')
