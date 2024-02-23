a338da1b7a44cecc8df9fb402ade9a733c71a92e
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-23 18:19:59 +0330

    add download_image func

--- Multi-Threading/005-ThreadPool/03-image-download.py
+++ Multi-Threading/005-ThreadPool/03-image-download.py
@@ -2,3 +2,17 @@ from concurrent.futures import ThreadPoolExecutor
 from urllib.request import urlopen
 import time
 import os
+
+
+def download_image(url):
+    image_data = None
+    with urlopen(url) as f:
+        image_data = f.read()
+
+    if not image_data:
+        raise Exception(f"Error: could not download the image from {url}")
+
+    filename = os.path.basename(url)
+    with open(filename, 'wb') as image_file:
+        image_file.write(image_data)
+        print(f'{filename} was downloaded...')
