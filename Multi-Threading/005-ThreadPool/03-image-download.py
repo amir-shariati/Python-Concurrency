9acfe844a3bea3100a0586e857f93d1c8fe94db3
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-23 18:20:58 +0330

    add ThreadPoolExecutor

--- Multi-Threading/005-ThreadPool/03-image-download.py
+++ Multi-Threading/005-ThreadPool/03-image-download.py
@@ -16,3 +16,13 @@ def download_image(url):
     with open(filename, 'wb') as image_file:
         image_file.write(image_data)
         print(f'{filename} was downloaded...')
+
+
+urls = ['https://upload.wikimedia.org/wikipedia/commons/9/9d/Python_bivittatus_1701.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/4/48/Python_Regius.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/d/d3/Baby_carpet_python_caudal_luring.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/f/f0/Rock_python_pratik.JPG',
+        'https://upload.wikimedia.org/wikipedia/commons/0/07/Dulip_Wilpattu_Python1.jpg']
+
+with ThreadPoolExecutor() as executor:
+      executor.map(download_image, urls)
