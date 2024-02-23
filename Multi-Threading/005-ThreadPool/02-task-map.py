4bc3db0a265fa060824d5959583557c864ee7749
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-23 18:00:23 +0330

    add ThreadPoolExecutor

--- Multi-Threading/005-ThreadPool/02-task-map.py
+++ Multi-Threading/005-ThreadPool/02-task-map.py
@@ -6,3 +6,9 @@ def task(id):
     print(f'Starting the task {id}...')
     sleep(1)
     return f'Done with task {id}'
+
+
+with ThreadPoolExecutor as executor:
+    results = executor.map(task, [1, 2])
+    for result in results:
+        print(result)
