3c7b0b2ac5b25fab14326e3cf3b9a1416972d342
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-23 17:54:54 +0330

    add task func

--- Multi-Threading/005-ThreadPool/02-task-map.py
+++ Multi-Threading/005-ThreadPool/02-task-map.py
@@ -1,3 +1,8 @@
 from time import sleep, perf_counter
 from concurrent.futures import ThreadPoolExecutor
 
+
+def task(id):
+    print(f'Starting the task {id}...')
+    sleep(1)
+    return f'Done with task {id}'
