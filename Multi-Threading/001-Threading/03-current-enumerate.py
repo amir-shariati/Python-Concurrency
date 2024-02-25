c879ed4415e15233feabca77d680e7ae8400c020
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-25 19:56:20 +0330

    add show func

--- Multi-Threading/001-Threading/03-current-enumerate.py
+++ Multi-Threading/001-Threading/03-current-enumerate.py
@@ -1,2 +1,8 @@
 from time import sleep, perf_counter
 from threading import Thread, current_thread, enumerate
+
+
+def show(name):
+    print(f'start {name}')
+    sleep(2)
+    print(f'stop {name}')
