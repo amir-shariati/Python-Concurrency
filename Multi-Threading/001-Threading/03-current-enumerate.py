0854c3c55f312c4a535735938f653a958c6eb512
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-25 19:56:55 +0330

    add t1, t2 threads

--- Multi-Threading/001-Threading/03-current-enumerate.py
+++ Multi-Threading/001-Threading/03-current-enumerate.py
@@ -6,3 +6,9 @@ def show(name):
     print(f'start {name}')
     sleep(2)
     print(f'stop {name}')
+
+
+start_time = perf_counter()
+
+t1 = Thread(target=show, args=('One',))
+t2 = Thread(target=show, args=('Two',))
