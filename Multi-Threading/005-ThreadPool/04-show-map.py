9cf79f202170c33f0221ce78e0c1287234ea2e5b
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-27 00:37:37 +0330

    add end_time and log times

--- Multi-Threading/005-ThreadPool/04-show-map.py
+++ Multi-Threading/005-ThreadPool/04-show-map.py
@@ -14,3 +14,7 @@ start_time = perf_counter()
 with ThreadPoolExecutor(max_workers=2) as executor:
     names = ['one', 'two', 'three', 'four', 'five', 'six', 'seven']
     executor.map(show, names)
+
+end_time = perf_counter()
+
+print(f'It took {end_time - start_time: 0.2f} second(S)')
