570f4f584f7eac24c077eb72f4e194f4be7e6d0c
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-28 00:55:26 +0330

    add Counter class

--- Multi-Threading/006-Lock/03-Counter.py
+++ Multi-Threading/006-Lock/03-Counter.py
@@ -1,2 +1,18 @@
 from time import sleep, perf_counter
 from threading import Thread, Lock
+
+
+class Counter:
+    def __init__(self):
+        self.value = 0
+        self.lock = Lock()
+
+    def increase(self, by):
+        with self.lock:
+            current_value = self.value
+            current_value += by
+
+            sleep(0.1)
+
+            self.value = current_value
+            print(f'counter={self.value}')
