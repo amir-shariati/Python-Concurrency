943ccef677c806afed7b7edf13de26968f12dcbe
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-28 01:05:31 +0330

    add main

--- Multi-Threading/006-Lock/04-Calculator.py
+++ Multi-Threading/006-Lock/04-Calculator.py
@@ -16,3 +16,21 @@ class Calculator:
         with self.lock:
             for _ in range(10000):
                 self.num -= 1
+
+
+def main():
+    calculator = Calculator()
+
+    t1 = Thread(target=calculator.add)
+    t2 = Thread(target=calculator.sub)
+
+    # start the threads
+    t1.start()
+    t2.start()
+
+    # wait for the threads to complete
+    t1.join()
+    t2.join()
+
+    print(f'num after run threads is: {calculator.num}')
+    print('Done')
