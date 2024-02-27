dddaedbc8ac033b02e5d9102a1faab3b9bac3de7
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-28 01:03:13 +0330

    add Calculator

--- Multi-Threading/006-Lock/04-Calculator.py
+++ Multi-Threading/006-Lock/04-Calculator.py
@@ -1,2 +1,18 @@
 from time import sleep, perf_counter
 from threading import Thread, Lock
+
+
+class Calculator:
+    def __init__(self):
+        self.num = 0
+        self.lock = Lock()
+
+    def add(self):
+        with self.lock:
+            for _ in range(10000):
+                self.num += 1
+
+    def sub(self):
+        with self.lock:
+            for _ in range(10000):
+                self.num -= 1
