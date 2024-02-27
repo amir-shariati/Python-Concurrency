2665ac9d73c818a6b3d3c9502b5f7691d77b99f1
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-28 01:17:54 +0330

    add call object

--- Multi-Threading/006-Lock/04-Calculator.py
+++ Multi-Threading/006-Lock/04-Calculator.py
@@ -15,8 +15,27 @@ class Calculator:
 
     def sub(self):
         with self.lock:
-            for _ in range(10000):
+            for _ in range(10):
                 self.num -= 1
+                print(f'in sub func, num is: {self.num}')
+
+    def __call__(self, *args, **kwargs):
+        print(f'call Calculator')
+        t1 = Thread(target=self.add)
+        t2 = Thread(target=self.sub)
+
+        # start the threads
+        t1.start()
+        t2.start()
+
+        print(f'threads run are: {enumerate()}')
+
+        # wait for the threads to complete
+        t1.join()
+        t2.join()
+
+        print(f'num after run threads is: {self.num}')
+        print('Done')
 
 
 def main():
