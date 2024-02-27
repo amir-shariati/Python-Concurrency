11cc6b7e5698303f770b125993f8d47898f60120
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-28 00:48:54 +0330

    add increase func

--- Multi-Threading/006-Lock/02-increase.py
+++ Multi-Threading/006-Lock/02-increase.py
@@ -1,2 +1,20 @@
 from time import sleep, perf_counter
 from threading import Thread, Lock
+
+counter = 0
+
+
+def increase(by, lock):
+    global counter
+
+    lock.acquire()
+
+    local_counter = counter
+    local_counter += by
+
+    sleep(0.1)
+
+    counter = local_counter
+    print(f'counter={counter}')
+
+    lock.release()
