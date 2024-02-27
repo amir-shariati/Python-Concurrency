20406e63d7eb96e5bb461e8176bc2749ba4cb1db
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-28 00:57:11 +0330

    add main func

--- Multi-Threading/006-Lock/03-Counter.py
+++ Multi-Threading/006-Lock/03-Counter.py
@@ -16,3 +16,20 @@ class Counter:
 
             self.value = current_value
             print(f'counter={self.value}')
+
+
+def main():
+    counter = Counter()
+    # create threads
+    t1 = Thread(target=counter.increase, args=(10, ))
+    t2 = Thread(target=counter.increase, args=(20, ))
+
+    # start the threads
+    t1.start()
+    t2.start()
+
+    # wait for the threads to complete
+    t1.join()
+    t2.join()
+
+    print(f'The final counter is {counter.value}')
