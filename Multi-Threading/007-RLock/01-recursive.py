e73a6c3f3be8f1bfa8b181bea98bb7f07cb37e4a
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-28 18:56:24 +0330

    add recursive_function

--- Multi-Threading/007-RLock/01-recursive.py
+++ Multi-Threading/007-RLock/01-recursive.py
@@ -4,3 +4,12 @@ from threading import Thread, RLock, current_thread
 lock = RLock()
 
 
+def recursive_function(count):
+    # Acquire the lock
+    lock.acquire()
+    print(f"Thread {current_thread().name} acquired lock: count={count}")
+    if count > 0:
+        # Call the function recursively
+        recursive_function(count - 1)
+        # Release the lock
+    lock.release()
