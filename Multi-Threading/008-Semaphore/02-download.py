d784a708e3970b1b691bbf159e14465eaaad5069
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-29 01:23:59 +0330

    add download func

--- Multi-Threading/008-Semaphore/02-download.py
+++ Multi-Threading/008-Semaphore/02-download.py
@@ -5,3 +5,15 @@ MAX_CONCURRENT_DOWNLOADS = 2
 semaphore = Semaphore(MAX_CONCURRENT_DOWNLOADS)
 
 
+def download(url):
+    with semaphore:
+        print(f"Downloading {url}...")
+
+        response = urllib.request.urlopen(url)
+        data = response.read()
+
+        print(f"Finished downloading {url}")
+
+        return data
+
+
