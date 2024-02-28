608c1d949a956bb268e1438a1bed3cfd038c6a5b
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-29 01:12:46 +0330

    add Semaphore

--- Multi-Threading/008-Semaphore/01-show.py
+++ Multi-Threading/008-Semaphore/01-show.py
@@ -1,2 +1,10 @@
 from threading import Thread, Semaphore, current_thread
 from time import sleep
+
+lock = Semaphore(value=2)
+
+
+def show():
+    with lock:
+        print(f'current thread is: {current_thread().getName()}')
+        sleep(2)
