aeac3d86291b8118f5f72cd1c9bcc8903fda6790
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-01 19:18:36 +0330

    add task func

--- Multi-Threading/010-Event/02-task.py
+++ Multi-Threading/010-Event/02-task.py
@@ -2,3 +2,14 @@ from threading import Thread, Event
 from time import sleep
 
 
+def task(event: Event) -> None:
+    for i in range(6):
+        print(f'Running #{i + 1}')
+        sleep(1)
+        if event.is_set():
+            print('The thread was stopped prematurely.')
+            break
+    else:
+        print('The thread was stopped maturely.')
+
+
