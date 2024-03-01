d727f7cee0f1941bd3ce32fb30c4bbec992b50b6
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-01 19:30:09 +0330

    add process_file func

--- Multi-Threading/010-Event/03-download.py
+++ Multi-Threading/010-Event/03-download.py
@@ -11,3 +11,21 @@ def download_file(url, event):
     event.set()
 
 
+def process_file(event):
+    print("Waiting for the file to be downloaded...")
+    event.wait()  # Wait for the event to be set
+
+    # File has been downloaded, start processing it
+    print("File download completed. Starting file processing...")
+
+    # Count the number of words in the file
+    word_count = 0
+    with open("rfc793.txt", "r") as file:
+        for line in file:
+            words = line.split()
+            word_count += len(words)
+
+    # Print the word count
+    print(f"Number of words in the file: {word_count}")
+
+
