646893fb2735810b13a9d58ccb4fbd40d098c08c
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-01 19:23:32 +0330

    add Worker class

--- Multi-Threading/010-Event/05-worker-stop.py
+++ Multi-Threading/010-Event/05-worker-stop.py
@@ -2,3 +2,18 @@ from threading import Thread, Event
 from time import sleep
 
 
+class Worker(Thread):
+        super().__init__(*args, **kwargs)
+        self.event = event
+
+    def run(self) -> None:
+        for i in range(6):
+            print(f'Running #{i + 1}')
+            sleep(1)
+            if self.event.is_set():
+                print('The thread was stopped prematurely.')
+                break
+        else:
+            print('The thread was stopped maturely.')
+
+
