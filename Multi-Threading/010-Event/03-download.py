da78a5fdc818d098bb8383f868d09e1109f907c6
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-01 19:29:39 +0330

    add download_file func

--- Multi-Threading/010-Event/03-download.py
+++ Multi-Threading/010-Event/03-download.py
@@ -2,3 +2,12 @@ from threading import Thread, Event
 from urllib import request
 
 
+def download_file(url, event):
+    # Download the file form URL
+    print(f"Downloading file from {url}...")
+    filename, _ = request.urlretrieve(url, "rfc793.txt")
+
+    # File download completed, set the event
+    event.set()
+
+
