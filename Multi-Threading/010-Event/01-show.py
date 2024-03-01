05edb653a7d760ced56cf6738b9c6c242b56b672
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-01 18:52:40 +0330

    add show_first func

--- Multi-Threading/010-Event/01-show.py
+++ Multi-Threading/010-Event/01-show.py
@@ -1,2 +1,11 @@
 from threading import Thread, Event, current_thread
 from time import sleep
+
+
+def show_first(f: Event, s: Event):
+    print(f'first is starting')
+    sleep(1)
+    f.set()
+    s.wait()
+    print(f'first is running')
+
