d256a5d5042062b9a671e301eb5a473640f655f3
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-02 23:40:33 +0330

    add images

--- Multi-Processing/001-Process/02-image-resize.py
+++ Multi-Processing/001-Process/02-image-resize.py
@@ -4,3 +4,12 @@ from PIL import Image, ImageFilter
 
 from multiprocessing import Process
 
+filenames = [
+    'images/1.jpg',
+    'images/2.jpg',
+    'images/3.jpg',
+    'images/4.jpg',
+    'images/5.jpg',
+]
+
+
