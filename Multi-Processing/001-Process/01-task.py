784ecd2be452e9bee559146ac8f134c5433ed585
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-02 23:32:18 +0330

    import time, Process

---
+++ Multi-Processing/001-Process/01-task.py
@@ -0,0 +1,25 @@
+from time import perf_counter
+from multiprocessing import Process
+
+
+def task() -> int:
+    result = 0
+    for _ in range(10**8):
+        result += 1
+    return result
+
+
+if __name__ == '__main__':
+    start = perf_counter()
+
+    p1 = Process(target=task)
+    p2 = Process(target=task)
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
+
+    finish = perf_counter()
+    print(f'It took {finish-start:.2f} second(s) to finish')
