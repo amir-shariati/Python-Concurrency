2d85358eb8673640534f30628216167cc2ce81f3
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-06 00:47:19 +0330

    add show func

--- Multi-Processing/001-Process/05-sub-process.py
+++ Multi-Processing/001-Process/05-sub-process.py
@@ -3,3 +3,12 @@ from multiprocessing import Process, current_process
 import os
 
 
+def show(name, delay):
+    print(f'starting {name}')
+    print(f'current_process is {current_process()}')
+    print(f'parent proces id of {current_process().name} is {os.getppid()}')
+    print(f'process id of {current_process().name} is {os.getpid()}')
+    time.sleep(delay)
+    print(f'finishing {name}')
+
+
