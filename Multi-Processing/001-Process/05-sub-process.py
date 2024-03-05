973c7ecd27a765216a8f9ff3d7d912b5da11cab4
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-06 00:48:59 +0330

    create two object class of ShowProcess class

--- Multi-Processing/001-Process/05-sub-process.py
+++ Multi-Processing/001-Process/05-sub-process.py
@@ -23,3 +23,12 @@ class ShowProcess(Process):
 
 
 start = time.perf_counter()
+
+p1 = ShowProcess(name='One', delay=2)
+p2 = ShowProcess(name='Two', delay=3)
+
+p1.start()
+p2.start()
+
+p1.join()
+p2.join()
