27819fa3e2ed41d49ff48b1b39cc035497cc41c8
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-06 00:29:48 +0330

    add show func

--- Multi-Processing/001-Process/04-process-attrs.py
+++ Multi-Processing/001-Process/04-process-attrs.py
@@ -1,3 +1,12 @@
 import time
 from multiprocessing import Process, current_process
 import os
+
+
+def show(name):
+    print(f'starting {name}')
+    print(f'current_process is {current_process()}')
+    print(f'parent proces is {os.getppid()}')
+    print(f'process is {os.getpid()}')
+    time.sleep(3)
+    print(f'finishing {name}')
