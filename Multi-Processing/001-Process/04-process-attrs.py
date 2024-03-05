5e4be99626a471565804f27a01017a52d1cbf63e
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-06 00:34:49 +0330

    edit log process name

--- Multi-Processing/001-Process/04-process-attrs.py
+++ Multi-Processing/001-Process/04-process-attrs.py
@@ -6,8 +6,8 @@ import os
 def show(name):
     print(f'starting {name}')
     print(f'current_process is {current_process()}')
-    print(f'parent proces is {os.getppid()}')
-    print(f'process is {os.getpid()}')
+    print(f'parent proces id of {current_process().name} is {os.getppid()}')
+    print(f'process id of {current_process().name} is {os.getpid()}')
     time.sleep(3)
     print(f'finishing {name}')
 
