e3aa86ffcb1335e308a8d10d381b4c04191a8dc3
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-11 23:40:00 +0330

    add two daemon processes

--- Multi-Processing/001-Process/06-daemon.py
+++ Multi-Processing/001-Process/06-daemon.py
@@ -15,3 +15,7 @@ start = time.perf_counter()
 p1 = Process(target=show, args=('One',))
 p2 = Process(target=show, args=('Two',))
 
+# Daemon
+p3 = Process(target=show, args=('Three',), daemon=True)
+p4 = Process(target=show, args=('Four',), daemon=True)
+
