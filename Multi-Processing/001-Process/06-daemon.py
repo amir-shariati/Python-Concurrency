a90fd2e25d7af851554e5fd98550226e135053ad
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-11 23:39:08 +0330

    add show func

--- Multi-Processing/001-Process/06-daemon.py
+++ Multi-Processing/001-Process/06-daemon.py
@@ -3,3 +3,10 @@ from multiprocessing import Process, current_process
 import sys
 
 
+def show(name):
+    print(f'starting {name}')
+    print(f'current_process is {current_process()}')
+    time.sleep(3)
+    print(f'finishing {name}')
+
+
