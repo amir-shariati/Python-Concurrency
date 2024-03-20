7d56638574bd1275195cbca7b8febcd84b14512b
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-20 23:02:21 +0330

    add show func

--- Multi-Processing/001-Process/07-terminate.py
+++ Multi-Processing/001-Process/07-terminate.py
@@ -1,2 +1,10 @@
 import time
 from multiprocessing import Process, current_process
+
+
+def show(name):
+    print(f'starting {name}')
+    print(f'current_process is {current_process()}')
+    time.sleep(3)
+    print(f'finishing {name}')
+
