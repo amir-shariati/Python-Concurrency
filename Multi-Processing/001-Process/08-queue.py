0d5d2c9b9c04d2130f75b7e187f6cd974a4f5250
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-21 23:57:32 +0330

    add p1_func

--- Multi-Processing/001-Process/08-queue.py
+++ Multi-Processing/001-Process/08-queue.py
@@ -4,3 +4,11 @@ from multiprocessing import Process, current_process, Queue
 numbers = []
 
 
+def p1_func(queue: Queue):
+    print(f'current_process is {current_process()}')
+    nums = queue.get()
+    nums.extend([1, 2, 3])
+    queue.put(nums)
+    print(f'p1 nums is: {nums}')
+
+
