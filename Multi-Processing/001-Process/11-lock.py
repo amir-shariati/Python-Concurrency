d9d3e3926620ed3bda0a18eef79e76b59a655957
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-22 01:10:39 +0330

    add p1_func

--- Multi-Processing/001-Process/11-lock.py
+++ Multi-Processing/001-Process/11-lock.py
@@ -2,3 +2,11 @@ import time
 from multiprocessing import Process, current_process, Lock
 
 
+def p1_func(num, lock: Lock):
+    print(f'current_process is {current_process()}')
+    with lock:
+        for _ in range(10000):
+            num += 1
+    print(f'finish process: {current_process()}')
+
+
