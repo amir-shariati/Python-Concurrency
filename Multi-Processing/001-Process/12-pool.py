9eb4dcc2aae9d287e1f7270050ef7e80abeeb83c
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-22 01:33:42 +0330

    add show

--- Multi-Processing/001-Process/12-pool.py
+++ Multi-Processing/001-Process/12-pool.py
@@ -2,3 +2,8 @@ import time
 from multiprocessing import Pool, current_process, cpu_count
 
 
+def show(name):
+    print(f'{name}, current_process is {current_process()}')
+    time.sleep(2)
+    print(f'{name}, finish process: {current_process()}')
+
