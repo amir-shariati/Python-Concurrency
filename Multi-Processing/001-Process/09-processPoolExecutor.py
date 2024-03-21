187aac983e973dcb80bb1d2b31158b9290a43f89
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-22 00:44:25 +0330

    add show

--- Multi-Processing/001-Process/09-processPoolExecutor.py
+++ Multi-Processing/001-Process/09-processPoolExecutor.py
@@ -3,3 +3,10 @@ from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import current_process
 
 
+def show(name):
+    print(f'starting {name}')
+    print(f'current_process is {current_process()}')
+    time.sleep(3)
+    print(f'finishing {name}')
+
+
