87bdf3f58a2f0394e36923aa488fe740e908b021
amirshariati <seyed.amir.shariati@gmail.com>
2024-03-22 01:10:58 +0330

    add p2_func

--- Multi-Processing/001-Process/11-lock.py
+++ Multi-Processing/001-Process/11-lock.py
@@ -10,3 +10,12 @@ def p1_func(num, lock: Lock):
     print(f'finish process: {current_process()}')
 
 
+def p2_func(num, lock: Lock):
+    print(f'current_process is {current_process()}')
+    lock.acquire()
+    for _ in range(10000):
+        num += 1
+    lock.release()
+    print(f'finish process: {current_process()}')
+
+
