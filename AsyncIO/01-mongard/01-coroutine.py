5c23ab4cb358670f313e3b756166e4f9560bcdf5
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-03 19:41:43 +0330

    run say_hello func

--- AsyncIO/01-mongard/01-coroutine.py
+++ AsyncIO/01-mongard/01-coroutine.py
@@ -8,6 +8,8 @@ async def say_hello(name: str):
     print(f'{datetime.datetime.now().time().strftime("%H:%M:%S")}, hello {name}')
 
 start = time.perf_counter()
+asyncio.run(say_hello('One'))
+asyncio.run(say_hello('Two'))
 end = time.perf_counter()
 
 print(f'It took {end - start:.2f} second(s) to finish')
