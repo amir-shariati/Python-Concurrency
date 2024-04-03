aeb3c54ac43777dcdfbb0491815ab6d03bd4499e
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-03 19:41:25 +0330

    add start, end time and log time

--- AsyncIO/01-mongard/01-coroutine.py
+++ AsyncIO/01-mongard/01-coroutine.py
@@ -7,3 +7,7 @@ async def say_hello(name: str):
     await asyncio.sleep(2)
     print(f'{datetime.datetime.now().time().strftime("%H:%M:%S")}, hello {name}')
 
+start = time.perf_counter()
+end = time.perf_counter()
+
+print(f'It took {end - start:.2f} second(s) to finish')
