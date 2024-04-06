c57c7eb7e5a8cdb7ab73430ddb280aa8d2ee8487
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-07 01:41:46 +0330

    add send_msg func

--- AsyncIO/01-mongard/03-cancel-Timeout.py
+++ AsyncIO/01-mongard/03-cancel-Timeout.py
@@ -2,3 +2,9 @@ import asyncio
 from asyncio import TimeoutError
 
 
+async def send_msg(msg, delay):
+    print(f'{asyncio.current_task().get_name()} is started')
+    await asyncio.sleep(delay)
+    print(f'{asyncio.current_task().get_name()} is Done')
+
+
