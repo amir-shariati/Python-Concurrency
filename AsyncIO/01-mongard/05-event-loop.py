957a47b8282a0254f874fcd54fb589d3443cc6de
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-07 15:43:05 +0330

    add new line

--- AsyncIO/01-mongard/05-event-loop.py
+++ AsyncIO/01-mongard/05-event-loop.py
@@ -1,6 +1,7 @@
 import asyncio
 import datetime
 
+
 async def send_msg(msg, delay):
     print(f'{datetime.datetime.now().time().strftime("%H:%M:%S")}, {asyncio.current_task().get_name()} is started')
     await asyncio.sleep(delay)
