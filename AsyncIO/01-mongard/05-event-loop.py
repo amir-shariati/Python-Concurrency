38d8b6de8de84442fd7f742ba3ad64ed70ee329b
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-07 15:40:58 +0330

    add send_msg coroutine

--- AsyncIO/01-mongard/05-event-loop.py
+++ AsyncIO/01-mongard/05-event-loop.py
@@ -1,2 +1,8 @@
 import asyncio
 import datetime
+
+async def send_msg(msg, delay):
+    print(f'{datetime.datetime.now().time().strftime("%H:%M:%S")}, {asyncio.current_task().get_name()} is started')
+    await asyncio.sleep(delay)
+    print(f'{datetime.datetime.now().time().strftime("%H:%M:%S")}, {asyncio.current_task().get_name()} is Done')
+
