183a1dbf24ee0604bb07887edcfc4a05f4f393e2
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-08 17:00:47 +0330

    add urls

--- AsyncIO/01-mongard/06-gather.py
+++ AsyncIO/01-mongard/06-gather.py
@@ -3,4 +3,14 @@ import datetime
 import time
 import aiohttp
 
+urls = [
+
+    'https://www.wikipedia.org/',
+    'https://en.wikipedia.org/wiki/Python_(programming_language)',
+    'https://en.wikipedia.org/wiki/Go_(programming_language)',
+    'https://en.wikipedia.org/wiki/Node.js',
+    'https://en.wikipedia.org/wiki/Rust_(programming_language)',
+]
+
+
 
