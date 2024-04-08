c895b5bd8359fecd3b8c40abcd71fcbd060899a1
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-08 20:29:45 +0330

    add set_after coroutine

--- AsyncIO/01-moon-tuts/07-future.py
+++ AsyncIO/01-moon-tuts/07-future.py
@@ -1,3 +1,12 @@
 import asyncio
 import datetime
 import time
+
+
+async def set_after(fut: asyncio.Future, delay, value):
+    print(f'{datetime.datetime.now().time().strftime("%H:%M:%S")}, {asyncio.current_task().get_name()} is started')
+    await asyncio.sleep(delay)
+    fut.set_result(value)
+    print(
+        f'{datetime.datetime.now().time().strftime("%H:%M:%S")}, {asyncio.current_task().get_name()}, set future:{value}')
+
