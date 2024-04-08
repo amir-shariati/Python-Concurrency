68c708a5160cf9863a889ce347f6e78dfa9fc6d0
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-08 20:52:17 +0330

    add urls

--- AsyncIO/01-moon-tuts/08-as_completed.py
+++ AsyncIO/01-moon-tuts/08-as_completed.py
@@ -2,3 +2,12 @@ import asyncio
 import datetime
 import time
 import aiohttp
+
+urls = [
+
+    'https://www.wikipedia.org/',
+    'https://en.wikipedia.org/wiki/Python_(programming_language)',
+    'https://en.wikipedia.org/wiki/Go_(programming_language)',
+    'https://en.wikipedia.org/wiki/Node.js',
+    'https://en.wikipedia.org/wiki/Rust_(programming_language)',
+]
