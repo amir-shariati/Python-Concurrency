731b18713bb694697b16bf1f33cca4081f8d38ab
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-09 15:31:17 +0330

    add show_status coroutine

--- AsyncIO/01-moon-tuts/09-wait.py
+++ AsyncIO/01-moon-tuts/09-wait.py
@@ -11,3 +11,15 @@ urls = [
     'https://en.wikipedia.org/wiki/Node.js',
     'https://en.wikipedia.org/wiki/Rust_(programming_language)',
 ]
+
+
+async def show_status(session: aiohttp.ClientSession, url: str, delay):
+    print(f'{datetime.datetime.now().time().strftime("%H:%M:%S")}, {asyncio.current_task().get_name()} is started')
+    await asyncio.sleep(delay)
+    async with session.get(url) as res:
+        print(
+            f'{datetime.datetime.now().time().strftime("%H:%M:%S")},'
+            f' {asyncio.current_task().get_name()}, '
+            f' delay:{delay}, status for url {url} is:{res.status}')
+        return res.status
+
