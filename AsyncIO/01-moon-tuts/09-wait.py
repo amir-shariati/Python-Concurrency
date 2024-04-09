2aac3cfd70695ef5ebae80a2d7c34b9207a4a928
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-09 15:13:19 +0330

    add urls

--- AsyncIO/01-moon-tuts/09-wait.py
+++ AsyncIO/01-moon-tuts/09-wait.py
@@ -4,3 +4,11 @@ import random
 import time
 import aiohttp
 
+urls = [
+
+    'https://www.wikipedia.org/',
+    'https://en.wikipedia.org/wiki/Python_(programming_language)',
+    'https://en.wikipedia.org/wiki/Go_(programming_language)',
+    'https://en.wikipedia.org/wiki/Node.js',
+    'https://en.wikipedia.org/wiki/Rust_(programming_language)',
+]
