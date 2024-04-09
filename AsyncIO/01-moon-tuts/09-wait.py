a881cfc6706b57f89af9a4f380c4de162d0d1048
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-09 15:30:49 +0330

    remove space

--- AsyncIO/01-moon-tuts/09-wait.py
+++ AsyncIO/01-moon-tuts/09-wait.py
@@ -5,7 +5,6 @@ import time
 import aiohttp
 
 urls = [
-
     'https://www.wikipedia.org/',
     'https://en.wikipedia.org/wiki/Python_(programming_language)',
     'https://en.wikipedia.org/wiki/Go_(programming_language)',
