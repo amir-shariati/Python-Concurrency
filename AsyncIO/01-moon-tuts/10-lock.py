559fe6a53ab12fcc39703a7eeb2e4aaed6f92951
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-09 19:11:44 +0330

    add main coroutine

--- AsyncIO/01-moon-tuts/10-lock.py
+++ AsyncIO/01-moon-tuts/10-lock.py
@@ -34,3 +34,25 @@ async def decrement(lock: asyncio.Lock):
         f'decrement, {asyncio.current_task().get_name()}, counter is: {counter}')
     lock.release()
 
+
+async def main():
+    global counter
+    lock = asyncio.Lock()
+
+    start = time.perf_counter()
+    print(f'start increment coroutine by gather')
+    tasks = [asyncio.create_task(increment(lock)) for _ in range(6)]
+    await asyncio.gather(*tasks)
+    end = time.perf_counter()
+    print(f'global counter is {counter}')
+    print(f'gather_client_session took {end - start:.2f} second(s) to finish')
+
+    print('-------------------------------------------------------------')
+
+    start = time.perf_counter()
+    print(f'start decrement coroutine by gather')
+    tasks = [asyncio.create_task(decrement(lock)) for _ in range(5)]
+    await asyncio.gather(*tasks)
+    end = time.perf_counter()
+    print(f'global counter is {counter}')
+    print(f'gather_client_session took {end - start:.2f} second(s) to finish')
