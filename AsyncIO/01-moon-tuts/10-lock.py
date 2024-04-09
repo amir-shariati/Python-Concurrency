ae05a229a345afb4ec26f6471c523c9f351e8125
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-09 19:11:08 +0330

    add increment coroutine

--- AsyncIO/01-moon-tuts/10-lock.py
+++ AsyncIO/01-moon-tuts/10-lock.py
@@ -4,3 +4,17 @@ import time
 
 counter = 0
 
+
+async def increment(lock: asyncio.Lock):
+    print(f'{datetime.datetime.now().time().strftime("%H:%M:%S")}, '
+          f'increment, {asyncio.current_task().get_name()} is started')
+    global counter
+    async with lock:
+        temp_counter = counter
+        temp_counter += 1
+        await asyncio.sleep(0.01)
+        counter = temp_counter
+        print(
+            f'{datetime.datetime.now().time().strftime("%H:%M:%S")}, '
+            f'increment, {asyncio.current_task().get_name()}, counter is: {counter}')
+
