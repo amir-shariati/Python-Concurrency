9fc19a0a67f5828bad921211604ffc4b188d8c94
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-10 18:24:01 +0330

    add urls

--- AsyncIO/01-moon-tuts/11-semaphore.py
+++ AsyncIO/01-moon-tuts/11-semaphore.py
@@ -5,3 +5,14 @@ import aiohttp
 import random
 import functools
 from aiodecorators import Semaphore
+
+
+urls = [
+    'https://www.wikipedia.org/',
+    'https://en.wikipedia.org/wiki/Python_(programming_language)',
+    'https://en.wikipedia.org/wiki/Go_(programming_language)',
+    'https://en.wikipedia.org/wiki/Node.js',
+    'https://en.wikipedia.org/wiki/Rust_(programming_language)',
+]
+
+
