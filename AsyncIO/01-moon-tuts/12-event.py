8b1ad58b6e44920d2d488a6544395ae83efa82ef
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-10 22:54:38 +0330

    add waiter coroutine

--- AsyncIO/01-moon-tuts/12-event.py
+++ AsyncIO/01-moon-tuts/12-event.py
@@ -9,3 +9,15 @@ def trigger(event: asyncio.Event):
     event.set()
 
 
+async def waiter(event: asyncio.Event, delay):
+    print(f'{datetime.datetime.now().time().strftime("%H:%M:%S")}, {asyncio.current_task().get_name()} is started')
+    await event.wait()
+    print(
+        f'{datetime.datetime.now().time().strftime("%H:%M:%S")},'
+        f' {asyncio.current_task().get_name()}, '
+        f' delay:{delay}, run processing')
+    await asyncio.sleep(delay)
+    print(
+        f'{datetime.datetime.now().time().strftime("%H:%M:%S")}, {asyncio.current_task().get_name()}, finish processing')
+
+
