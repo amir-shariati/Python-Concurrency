db9830917f40909d7e1c1f2cbea631c4eda96f72
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-11 16:02:11 +0330

    add trigger coroutine

--- AsyncIO/01-moon-tuts/13-condition.py
+++ AsyncIO/01-moon-tuts/13-condition.py
@@ -4,3 +4,14 @@ import time
 import random
 
 
+async def trigger(condition: asyncio.Condition):
+    print(f'{datetime.datetime.now().time().strftime("%H:%M:%S")}, {asyncio.current_task().get_name()} is started')
+    await asyncio.sleep(5)
+
+    async with condition:
+        print('-------------------------------------------------------------')
+        print(f'{datetime.datetime.now().time().strftime("%H:%M:%S")}, {asyncio.current_task().get_name()}, Notify all tasks')
+        condition.notify_all()
+        print('-------------------------------------------------------------')
+
+
