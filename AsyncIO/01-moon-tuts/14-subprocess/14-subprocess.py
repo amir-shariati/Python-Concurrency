4938f52e4c586aa5dcc8854f9ef8b241c33eff49
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-12 16:59:23 +0330

    log time

--- AsyncIO/01-moon-tuts/14-subprocess/14-subprocess.py
+++ AsyncIO/01-moon-tuts/14-subprocess/14-subprocess.py
@@ -5,6 +5,9 @@ import time
 
 async def main():
 
+    start = time.perf_counter()
+    print(f'start create_subprocess_exec coroutine')
+
     process = await asyncio.create_subprocess_exec(
         'python',
         '14-username.py',
@@ -13,6 +16,12 @@ async def main():
     )
 
     stdout, stderr = await process.communicate(b'Amir Shariati')
+    end = time.perf_counter()
+
     print(f'{datetime.datetime.now().time().strftime("%H:%M:%S")}, stdout: {stdout}')
     print(f'{datetime.datetime.now().time().strftime("%H:%M:%S")}, stderr: {stderr}')
+
+    print('-------------------------------------------------------------')
+    print(f'waiter coroutine took {end - start:.2f} second(s) to finish')
+
 
