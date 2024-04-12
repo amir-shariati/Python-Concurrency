5d9d05b3223e06820a57f80cfe0648269130572f
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-12 16:57:55 +0330

    add create_subprocess_exec coroutine

--- AsyncIO/01-moon-tuts/14-subprocess/14-subprocess.py
+++ AsyncIO/01-moon-tuts/14-subprocess/14-subprocess.py
@@ -5,3 +5,10 @@ import time
 
 async def main():
 
+    process = await asyncio.create_subprocess_exec(
+        'python',
+        '14-username.py',
+        stdin=asyncio.subprocess.PIPE,
+        stdout=asyncio.subprocess.PIPE
+    )
+
